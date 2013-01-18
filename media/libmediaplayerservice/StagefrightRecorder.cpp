9bc41facfe1c99c4a17dfad3272b1300cc02cb08
Hua Wang <Hua.Wang@windriver.com>
2013-01-18 18:51:13 +0800

    CameraSource: set up right frame rate
    If camera recorder uses HW codec, the capabilities of the codec
    are from media_profile.xml; If using SW codec, it is invisible
    to the applications, In other words, the applications won't be
    able to use the codec or query the capabilities of the codec
    at all, it only gives some ranges. StagefrightRecorder will
    use default parameters to configure camera, as for framerate,
    it should be from camera HAL feedback.
    
    Issue:AXIA-1446
    Change-Id: I77bf41239a19d26e2da1c28258288fdaa1c98297
    Signed-off-by: Hua Wang <Hua.Wang@windriver.com>

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -217,6 +217,7 @@ status_t StagefrightRecorder::setVideoFrameRate(int frames_per_second) {
 
     // Additional check on the frame rate will be performed later
     mFrameRate = frames_per_second;
+    mUserSetupFrameRate = true;
 
     return OK;
 }
@@ -1376,7 +1377,7 @@ status_t StagefrightRecorder::setupCameraSource(
 
     // When frame rate is not set, the actual frame rate will be set to
     // the current frame rate being used.
-    if (mFrameRate == -1) {
+    if (mFrameRate == -1 || !mUserSetupFrameRate) {
         int32_t frameRate = 0;
         CHECK ((*cameraSource)->getFormat()->findInt32(
                     kKeyFrameRate, &frameRate));
@@ -1716,6 +1717,7 @@ status_t StagefrightRecorder::reset() {
     mCameraSourceTimeLapse = NULL;
     mIsMetaDataStoredInVideoBuffers = false;
     mEncoderProfiles = MediaProfiles::getInstance();
+    mUserSetupFrameRate = false;
     mRotationDegrees = 0;
     mLatitudex10000 = -3600000;
     mLongitudex10000 = -3600000;
