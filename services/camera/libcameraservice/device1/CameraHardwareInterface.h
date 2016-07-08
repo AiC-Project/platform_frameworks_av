3a6dcc04c9fde72aa099352d9c4709ef028c9dc2
Mathieu Pasquet <mathieu.pasquet@alterway.fr>
2016-07-08 12:02:55 +0200

    Disable HAL stopPreview() function
    Workaround for a weird bug in AiC

--- services/camera/libcameraservice/device1/CameraHardwareInterface.h
+++ services/camera/libcameraservice/device1/CameraHardwareInterface.h
@@ -217,9 +217,11 @@ public:
      */
     void stopPreview()
     {
-        ALOGV("%s(%s)", __FUNCTION__, mName.string());
+        ALOGV("%s(%s) (disabled)", __FUNCTION__, mName.string());
+        /*
         if (mDevice->ops->stop_preview)
             mDevice->ops->stop_preview(mDevice);
+        */
     }
 
     /**
