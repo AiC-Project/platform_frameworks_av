darren-clark/android_platform_frameworks_av

libstagefright: add ffmpeg components

media/libstagefright/omx/SoftOMXPlugin.cpp
@@ -34,6 +34,27 @@ static const struct {
     const char *mRole;
 
 } kComponents[] = {
+    { "OMX.ffmpeg.h264.decoder", "ffmpegvdec", "video_decoder.avc" },
+    { "OMX.ffmpeg.mpeg4.decoder", "ffmpegvdec", "video_decoder.mpeg4" },
+    { "OMX.ffmpeg.mpeg2v.decoder", "ffmpegvdec", "video_decoder.mpeg2v" },
+    { "OMX.ffmpeg.h263.decoder", "ffmpegvdec", "video_decoder.h263" },
+    { "OMX.ffmpeg.vpx.decoder", "ffmpegvdec", "video_decoder.vpx" },
+    { "OMX.ffmpeg.vc1.decoder", "ffmpegvdec", "video_decoder.vc1" },
+    { "OMX.ffmpeg.divx.decoder", "ffmpegvdec", "video_decoder.divx" },
+    { "OMX.ffmpeg.wmv.decoder", "ffmpegvdec", "video_decoder.wmv" },
+    { "OMX.ffmpeg.flv.decoder", "ffmpegvdec", "video_decoder.flv" },
+    { "OMX.ffmpeg.rv.decoder", "ffmpegvdec", "video_decoder.rv" },
+    { "OMX.ffmpeg.mp1.decoder", "ffmpegadec", "audio_decoder.mp1" },
+    { "OMX.ffmpeg.mp2.decoder", "ffmpegadec", "audio_decoder.mp2" },
+    { "OMX.ffmpeg.mp3.decoder", "ffmpegadec", "audio_decoder.mp3" },
+    { "OMX.ffmpeg.aac.decoder", "ffmpegadec", "audio_decoder.aac" },
+    { "OMX.ffmpeg.ac3.decoder", "ffmpegadec", "audio_decoder.ac3" },
+    { "OMX.ffmpeg.wma.decoder", "ffmpegadec", "audio_decoder.wma" },
+    { "OMX.ffmpeg.ra.decoder", "ffmpegadec", "audio_decoder.ra" },
+    { "OMX.ffmpeg.ape.decoder", "ffmpegadec", "audio_decoder.ape" },
+    { "OMX.ffmpeg.dts.decoder", "ffmpegadec", "audio_decoder.dts" },
+    { "OMX.ffmpeg.flac.decoder", "ffmpegadec", "audio_decoder.flac" },
+    { "OMX.ffmpeg.vorbis.decoder", "ffmpegadec", "audio_decoder.vorbis" },
     { "OMX.google.aac.decoder", "aacdec", "audio_decoder.aac" },
     { "OMX.google.aac.encoder", "aacenc", "audio_encoder.aac" },
     { "OMX.google.amrnb.decoder", "amrdec", "audio_decoder.amrnb" },

AiC-Project/platform_frameworks_av

media/libstagefright/omx/SoftOMXPlugin.cpp
@@ -34,6 +34,27 @@ static const struct {
     const char *mRole;
 
 } kComponents[] = {
+    { "OMX.ffmpeg.h264.decoder", "ffmpegvdec", "video_decoder.avc" },
+    { "OMX.ffmpeg.mpeg4.decoder", "ffmpegvdec", "video_decoder.mpeg4" },
+    { "OMX.ffmpeg.mpeg2v.decoder", "ffmpegvdec", "video_decoder.mpeg2v" },
+    { "OMX.ffmpeg.h263.decoder", "ffmpegvdec", "video_decoder.h263" },
+    { "OMX.ffmpeg.vp8.decoder", "ffmpegvdec", "video_decoder.vp8" },
+    { "OMX.ffmpeg.vc1.decoder", "ffmpegvdec", "video_decoder.vc1" },
+    { "OMX.ffmpeg.divx.decoder", "ffmpegvdec", "video_decoder.divx" },
+    { "OMX.ffmpeg.wmv.decoder", "ffmpegvdec", "video_decoder.wmv" },
+    { "OMX.ffmpeg.flv.decoder", "ffmpegvdec", "video_decoder.flv" },
+    { "OMX.ffmpeg.rv.decoder", "ffmpegvdec", "video_decoder.rv" },
+    { "OMX.ffmpeg.mp1.decoder", "ffmpegadec", "audio_decoder.mp1" },
+    { "OMX.ffmpeg.mp2.decoder", "ffmpegadec", "audio_decoder.mp2" },
+    { "OMX.ffmpeg.mp3.decoder", "ffmpegadec", "audio_decoder.mp3" },
+    { "OMX.ffmpeg.aac.decoder", "ffmpegadec", "audio_decoder.aac" },
+    { "OMX.ffmpeg.ac3.decoder", "ffmpegadec", "audio_decoder.ac3" },
+    { "OMX.ffmpeg.wma.decoder", "ffmpegadec", "audio_decoder.wma" },
+    { "OMX.ffmpeg.ra.decoder", "ffmpegadec", "audio_decoder.ra" },
+    { "OMX.ffmpeg.ape.decoder", "ffmpegadec", "audio_decoder.ape" },
+    { "OMX.ffmpeg.dts.decoder", "ffmpegadec", "audio_decoder.dts" },
+    { "OMX.ffmpeg.flac.decoder", "ffmpegadec", "audio_decoder.flac" },
+    { "OMX.ffmpeg.vorbis.decoder", "ffmpegadec", "audio_decoder.vorbis" },
     { "OMX.google.aac.decoder", "aacdec", "audio_decoder.aac" },
     { "OMX.google.aac.encoder", "aacenc", "audio_encoder.aac" },
     { "OMX.google.amrnb.decoder", "amrdec", "audio_decoder.amrnb" },